71876b82fbf775ddd9fa578ac55fb2a6ddc8aa33
Joo Won Lee <andrewajsk@gmail.com>
2017-07-31 20:59:17 -0500

    database done

--- src/Common/NewPost.js
+++ src/Common/NewPost.js
@@ -9,9 +9,9 @@ class NewPost extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			Title: '',
+			Title: 'TITLE',
 			Pics: '',
-			Price: '',
+			Price: 0,
 		}
 		this.handleChangeTitle = this.handleChangeTitle.bind(this);
 		this.handleChangePics = this.handleChangePics.bind(this);
@@ -23,7 +23,9 @@ class NewPost extends Component{
 
 	}
 
-	addPost (){
+	addPost (e){
+    e.preventDefault();
+
 		const rootRef = firebase.database().ref().child('Buy');
 		var item = {
 			Title: this.state.Title,
@@ -31,6 +33,12 @@ class NewPost extends Component{
 			PostedDate: new Date().toString(),
 			Price: this.state.Price,
 			Availability: true
+
+  //     Title: "TEST",
+  //     Pics: "TESTING NONE",
+  //     PostedDate: new Date().toString(),
+  //     Price: 50,
+  //     Availability: true
 		}
 		rootRef.push(item);
 		console.log("PUSHED WTF");
@@ -38,14 +46,17 @@ class NewPost extends Component{
 
 	handleChangeTitle(event){
 		this.setState({Title: event.target.value});
+    console.log("TITLE DONE");
 	}
 
 	handleChangePics(event){
 		this.setState({Pics: event.target.value});
+    console.log("PICS DONE");
 	}
 
 	handleChangePrice(event){
 		this.setState({Price: event.target.value});
+    console.log("PRICE DONE");
 	}
 
 	handleSubmit(event){
@@ -56,7 +67,7 @@ class NewPost extends Component{
 
 		return(
 			<div>
-				<form onSubmit={this.addPost} className="form-horizontal center-children">
+				<form onSubmit={this.addPost}>
 					<div className="container">
 						<div>
 						  	<label><b>Title*</b></label>
@@ -70,13 +81,13 @@ class NewPost extends Component{
 
 						<div>
 						    <label><b>Desirable Price*</b></label>
-						    <input type="password" placeholder="Enter Password" id="price" name="password" value={this.state.Price} onChange={this.handleChangePrice} required/>
+						    <input type="text" pattern="[0-9]*" placeholder="Enter Password" id="price" name="password" value={this.state.Price} onChange={this.handleChangePrice} required/>
 						</div>
-
+            
 				    <div>
 				    	<ButtonToolbar>
-							<Button type="submit" value="Submit" bsStyle="primary">Post</Button>
-							<Button>Cancel</Button>
+  							<Button type="submit" value="Submit"  bsStyle="primary">Post</Button>
+                <Button>Cancel</Button>
 					    </ButtonToolbar>
 			    	</div>
 					</div>
@@ -84,6 +95,61 @@ class NewPost extends Component{
 			</div>
 		)
 	}
+}
+
+class ImageUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {file: '',imagePreviewUrl: ''};
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+    console.log('handle uploading-', this.state.file);
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  render() {
+    let {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = (<img src={imagePreviewUrl} />);
+    } else {
+      $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
+    }
+
+    return (
+      <div className="previewComponent">
+        <form onSubmit={(e)=>this._handleSubmit(e)}>
+          <input className="fileInput" 
+            type="file" 
+            onChange={(e)=>this._handleImageChange(e)} />
+          <button className="submitButton" 
+            type="submit" 
+            onClick={(e)=>this._handleSubmit(e)}>Upload Image</button>
+        </form>
+        <div className="imgPreview">
+          {$imagePreview}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default NewPost;
