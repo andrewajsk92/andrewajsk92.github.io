aa75f8f0ea33937ab7010f159578d403ff5d2ad7
Joo Won Lee <andrewajsk@gmail.com>
2017-08-01 21:13:04 -0500

    Better version of push along with a little bit of picture upload

--- src/Common/NewPost.js
+++ src/Common/NewPost.js
@@ -9,13 +9,12 @@ class NewPost extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			Title: 'TITLE',
-			Pics: '',
-			Price: 0,
+			Title: '',
+			Price: ''
 		}
 		this.handleChangeTitle = this.handleChangeTitle.bind(this);
-		this.handleChangePics = this.handleChangePics.bind(this);
 		this.handleChangePrice = this.handleChangePrice.bind(this);
+    this.handleChangeImage = this.handleChangeImage.bind(this);
 
 		this.handleSubmit = this.handleSubmit.bind(this);
 
@@ -29,7 +28,6 @@ class NewPost extends Component{
 		const rootRef = firebase.database().ref().child('Buy');
 		var item = {
 			Title: this.state.Title,
-			Pics: this.state.Pics,
 			PostedDate: new Date().toString(),
 			Price: this.state.Price,
 			Availability: true
@@ -49,67 +47,12 @@ class NewPost extends Component{
     console.log("TITLE DONE");
 	}
 
-	handleChangePics(event){
-		this.setState({Pics: event.target.value});
-    console.log("PICS DONE");
-	}
-
 	handleChangePrice(event){
 		this.setState({Price: event.target.value});
     console.log("PRICE DONE");
 	}
 
-	handleSubmit(event){
-		console.log("WUT");
-	}
-
-	render(){
-
-		return(
-			<div>
-				<form onSubmit={this.addPost}>
-					<div className="container">
-						<div>
-						  	<label><b>Title*</b></label>
-						    <input type="text" placeholder="Enter Title" value={this.state.Title} onChange={this.handleChangeTitle} required/>
-						</div>
-						
-						<div>
-						    <label><b>Pics</b></label>
-						    <input type="text" placeholder="Enter Institution" id="pics" name="institution" value={this.state.Pics} onChange={this.handleChangePics}/>
-						</div>
-
-						<div>
-						    <label><b>Desirable Price*</b></label>
-						    <input type="text" pattern="[0-9]*" placeholder="Enter Password" id="price" name="password" value={this.state.Price} onChange={this.handleChangePrice} required/>
-						</div>
-            
-				    <div>
-				    	<ButtonToolbar>
-  							<Button type="submit" value="Submit"  bsStyle="primary">Post</Button>
-                <Button>Cancel</Button>
-					    </ButtonToolbar>
-			    	</div>
-					</div>
-				</form>
-			</div>
-		)
-	}
-}
-
-class ImageUpload extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {file: '',imagePreviewUrl: ''};
-  }
-
-  _handleSubmit(e) {
-    e.preventDefault();
-    // TODO: do something with -> this.state.file
-    console.log('handle uploading-', this.state.file);
-  }
-
-  _handleImageChange(e) {
+  handleChangeImage(e) {
     e.preventDefault();
 
     let reader = new FileReader();
@@ -125,7 +68,11 @@ class ImageUpload extends React.Component {
     reader.readAsDataURL(file)
   }
 
-  render() {
+	handleSubmit(event){
+		console.log("WUT");
+	}
+
+	render(){
     let {imagePreviewUrl} = this.state;
     let $imagePreview = null;
     if (imagePreviewUrl) {
@@ -134,23 +81,96 @@ class ImageUpload extends React.Component {
       $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
     }
 
-    return (
-      <div className="previewComponent">
-        <form onSubmit={(e)=>this._handleSubmit(e)}>
-          <input className="fileInput" 
-            type="file" 
-            onChange={(e)=>this._handleImageChange(e)} />
-          <button className="submitButton" 
-            type="submit" 
-            onClick={(e)=>this._handleSubmit(e)}>Upload Image</button>
-        </form>
-        <div className="imgPreview">
-          {$imagePreview}
-        </div>
-      </div>
-    )
-  }
+		return(
+			<div>
+				<form onSubmit={this.addPost}>
+					<div className="container">
+						<div>
+					  	<label><b>Title*</b></label>
+					    <input type="text" placeholder="Enter Title" value={this.state.Title} onChange={this.handleChangeTitle} required/>
+						</div>
+
+						<div>
+					    <label><b>Desirable Price*</b></label>
+					    <input type="text" pattern="[0-9]*" placeholder="Enter Password" id="price" name="password" value={this.state.Price} onChange={this.handleChangePrice} required/>
+						</div>
+
+            <div>
+              <input type="file" onChange={(e)=>this.handleChangeImage(e)} />
+            </div>
+
+				    <div>
+				    	<ButtonToolbar>
+  							<Button type="submit" value="Submit"  bsStyle="primary">Post</Button>
+                <Button>Cancel</Button>
+					    </ButtonToolbar>
+			    	</div>
+
+            <div className="imgPreview">
+              {$imagePreview}
+            </div>
+
+					</div>
+				</form>
+			</div>
+		)
+	}
 }
+
+// class ImageUpload extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {file: '',imagePreviewUrl: ''};
+//   }
+
+//   _handleSubmit(e) {
+//     e.preventDefault();
+//     // TODO: do something with -> this.state.file
+//     console.log('handle uploading-', this.state.file);
+//   }
+
+//   _handleImageChange(e) {
+//     e.preventDefault();
+
+//     let reader = new FileReader();
+//     let file = e.target.files[0];
+
+//     reader.onloadend = () => {
+//       this.setState({
+//         file: file,
+//         imagePreviewUrl: reader.result
+//       });
+//     }
+
+//     reader.readAsDataURL(file)
+//   }
+
+//   render() {
+//     let {imagePreviewUrl} = this.state;
+//     let $imagePreview = null;
+//     if (imagePreviewUrl) {
+//       $imagePreview = (<img src={imagePreviewUrl} />);
+//     } else {
+//       $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
+//     }
+
+//     return (
+//       <div className="previewComponent">
+//         <form onSubmit={(e)=>this._handleSubmit(e)}>
+//           <input className="fileInput" 
+//             type="file" 
+//             onChange={(e)=>this._handleImageChange(e)} />
+//           <button className="submitButton" 
+//             type="submit" 
+//             onClick={(e)=>this._handleSubmit(e)}>Upload Image</button>
+//         </form>
+//         <div className="imgPreview">
+//           {$imagePreview}
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 
 export default NewPost;
 
